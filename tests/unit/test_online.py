ec177c332a4d18be51970e4e02dbbc84c5423642
Marcello Perathoner <marcello@perathoner.de>
2022-10-14 12:21:10 +0200

    One more test

---
+++ tests/unit/test_online.py
@@ -0,0 +1,23 @@
+""" Test the Ukkonen "online" builder. """
+
+# pylint: disable=missing-docstring
+
+import pytest
+
+from suffix_tree import Tree
+from suffix_tree.builder_factory import BUILDERS
+
+
+class TestOnline:
+    def test_online(self):
+        """ Test building Ukkonen from an iterator."""
+        tree = Tree()
+
+        def generator():
+            yield from "xabxac"
+            assert tree.find("xac")  # tree is always ready
+            yield from "abc"
+
+        tree.add("A", generator(), builder="ukkonen")
+        assert tree.find("xacabc")
+
